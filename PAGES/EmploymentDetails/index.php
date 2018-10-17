939646d614bc109564ec59dd898a1ec0995bfb70
NBvinay <41563618+NBvinay@users.noreply.github.com>
2018-10-17 15:11:07 +0530

    Update index.php

--- PAGES/EmploymentDetails/index.php
+++ PAGES/EmploymentDetails/index.php
@@ -1,6 +1,6 @@
 <?php
    $con = mysqli_connect("localhost", "root", "", "govtdb");
-
+ 
   if(mysqli_connect_errno()) 
   {
     echo "Failed to connect: " . mysqli_connect_errno();
@@ -16,27 +16,41 @@
     // header("Location: register.php");
   }
 
+
+
+
+
   if(isset($_POST['submit']))
   {
+      
       $adharNumber = $_POST['adharNumber'];
       $companyName = $_POST['companyName'];
       $job = $_POST['job'];
       $salary = $_POST['salary'];
       $doj = $_POST['doj'];
-	  $status = $_POST['status'];
-	  $dor = $_POST['dor'];
+      $status = $_POST['status'];
+      $dor = $_POST['dor'];
       $flag = 1;
+      $doj1 = date("Y-m-d",strtotime($doj));
+      $dor1 = date("Y-m-d",strtotime($dor));
+      
+         
  
         
         if (!($companyName== NULL || $job== NULL || $salary== NULL || $doj== NULL ||$status== NULL ||$dor== NULL || $adharNumber==NULL))
         {
          
-          // Nigga Change acc to db
-		  //mysqli_query($con , "INSERT INTO `employment_details` (`Aadhar Number`, `Company Name`, `Job`, `Salary`, `Date_of_joining`, `Status`, `Date of resignation/retirement`) VALUES ('$adharNumber', '$companyName', '$job', '$salary', '$doj', '$status', '$dor');");
+          
+
+		      $res=mysqli_query($con,"INSERT INTO `employment details` (`Aadhar Number`,`Company Name`,`Job`,`Salary`,`Date_of_joining`,`Status`,`Date of resignation/retirement`) VALUES ('$adharNumber', '$companyName','$job', '$salary', '$doj1', '$status', '$dor1');");
           $flag = 0;
           
         }
-        
+
+        if(!$res)
+        {
+          echo "failed to insert";
+        }
         
       
       if($flag == 1)
@@ -65,8 +79,8 @@
 <div class="w3-sidebar w3-bar-block w3-border-right" style="display:none" id="mySidebar">
   <button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>
   <a href="index.php" class="w3-bar-item w3-button"><h2>Insert Data</h2></a>
-  <a href="updateVehicle.php" class="w3-bar-item w3-button"><h2>Update Data</h2></a>
-  <a href="DeleteVehicle.php" class="w3-bar-item w3-button"><h2>Delete Data</h2></a>
+  <a href="updateEmployee.php" class="w3-bar-item w3-button"><h2>Update Data</h2></a>
+  <a href="DeleteEmployee.php" class="w3-bar-item w3-button"><h2>Delete Data</h2></a>
 </div>
 
 <!-- Page Content -->
@@ -82,7 +96,7 @@
       <div class="tab-content">   
           <h1><b><b><div style="color:white;">Insert Employment Details</div></b></b></h1>
           
-          <form action="/" method="post">
+          <form action="index.php" method="POST">
 
                 <div class="field-wrap">
                       <label>
@@ -93,17 +107,12 @@
 				
 				
 				
-				<div class="field-wrap">
-                                  <label>
-                                        Company Name<span class="req">*</span>
-                                  </label>
-                              <input type="text"required autocomplete="off" name="companyName"/>
+				        <div class="field-wrap">
+                        <label>
+                              Company Name<span class="req">*</span>
+                        </label>
+                        <input type="text"required autocomplete="off" name="companyName"/>
                 </div>
-
-
-
-                
-                
 
                 <div class="input_fields_wrap">    
                    <div>
@@ -126,37 +135,29 @@
                         </div>
                       
                       
-                      <div class="field-wrap">
+                        <div class="field-wrap">
+                                  
+                                        <div style="color: white"> Date of Joining:</div>
+                                  
+                              <input type="Date" required autocomplete="off" name="doj"/>
+
+
+                        </div>
+  					  
+  					            <div class="field-wrap">
                                 <label>
-                                       Date of Joining(YYYY/MM/DD)<span class="req">*</span>
+                                     Status<span class="req">*</span>
                                 </label>
-                            <input type="text"required autocomplete="off" name="doj"/>
-
-
-                      </div>
-					  
-					  <div class="field-wrap">
-                              <label>
-                                   Status<span class="req">*</span>
-                              </label>
-                              <input type="text"required autocomplete="off" name="status"/>
-                        </div>
-                    
-					<div class="field-wrap">
-                              <label>
-                                   Date of Resignation/Retirement(YYYY/MM/DD)<span class="req"></span>
-                              </label>
-                              <input type="text"required autocomplete="off" name="dor"/>
-                        </div>
+                                <input type="text"required autocomplete="off" name="status"/>
+                          </div>
+                      
+  					             <div class="field-wrap">
+                                <div style="color: white"> Date of Retirement:</div>
+                                <input type="date"required autocomplete="off" name="dor"/>
+                          </div>
                     </div>
                 </div>
-            
-                
-                <button type="submit" class="button button-block"/>Submit Details</button>
-
-
-
-
+                <button type="submit" class="button button-block" name="submit" />Submit Details</button>
           </form>
 
         </div>
@@ -165,8 +166,8 @@
 
 </script>
         
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script  src="js/index.js"></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script  src="js/index.js"></script>
 
 
 
@@ -177,40 +178,6 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js">  </script>
     
-  
-<script>
-  $(document).ready(function() 
-  {
-    var max_fields      = 10; //maximum input boxes allowed
-    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
-    var add_button      = $(".add_field_button"); //Add button ID
-   
-    var x = 1; //initlal text box count
-  
-  
-   $(add_button).click(function(e)
-   { //on add input button click
-        e.preventDefault();
-        if(x < max_fields){ //max input box allowed
-  
-         //text box increment
-            $(wrapper).append('<div><button class="remove_field" style="background-color:#ff5230;"><b><b> -</b></b></button><div class="field-wrap"><input type="text"required autocomplete="off" placeholder=" 2/4 wheeler ?"/> </div> <div class="field-wrap"><input type="text"required autocomplete="off" placeholder= "Registration Number"/> </div> <div class="field-wrap"><input type="text"required autocomplete="off" placeholder=" Insurance Number"/> <br><br><br></div> </div> '); //add input box
-            x++; 
-    }
-    });
-   
-    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-       
-    e.preventDefault(); 
-    $(this).parent('div').remove(); 
-    x--;
-    })
-});
-  
-</script>
-
-
-
 <script>
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
